d27aca08e83cb1809ac245fc7b6889294f37a157
Peter Eisentraut <peter@eisentraut.org>
2015-11-12 06:49:51 -0500

    Fix behavior with nested attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,5 @@
+require 'chef/mixin/deep_merge'
+
 nodes_bag = begin
               data_bag('nodes')
             rescue Chef::Exceptions::InvalidDataBagPath, Net::HTTPServerException
@@ -9,17 +11,15 @@ if nodes_bag.include?(node.name)
 
   %w(default force_default override force_override).each do |attr_type|
     attrs = dbi["#{attr_type}_attributes"] || {}
-    attrs.each do |key, value|
-      case attr_type
-      when 'default'
-        node.default[key] = value
-      when 'force_default'
-        node.force_default[key] = value
-      when 'override'
-        node.override[key] = value
-      when 'force_override'
-        node.force_override[key] = value
-      end
+    case attr_type
+    when 'default'
+      node.default = Chef::Mixin::DeepMerge.merge(node.default, attrs)
+    when 'force_default'
+      node.force_default = Chef::Mixin::DeepMerge.merge(node.force_default, attrs)
+    when 'override'
+      node.override = Chef::Mixin::DeepMerge.merge(node.override, attrs)
+    when 'force_override'
+      node.force_override = Chef::Mixin::DeepMerge.merge(node.force_override, attrs)
     end
   end
 end
--- test/integration/some-missing/bats/some-missing.bats
+++ test/integration/some-missing/bats/some-missing.bats
@@ -1,5 +1,5 @@
 @test "correct attribute values processed by template" {
-	grep -F 'foo1 = ' /var/tmp/node-data.txt
+	grep -F 'foo1a = ' /var/tmp/node-data.txt
 	grep -F 'foo2 = bar2' /var/tmp/node-data.txt
 	grep -F 'foo3 = ' /var/tmp/node-data.txt
 	grep -F 'foo4 = ' /var/tmp/node-data.txt
